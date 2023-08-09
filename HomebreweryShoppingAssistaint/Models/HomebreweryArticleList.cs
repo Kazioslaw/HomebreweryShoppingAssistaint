becf1001b80891fe511a8f31cc7c0eb0160ce5e1
Kazioslaw <113318007+Kazioslaw@users.noreply.github.com>
2023-08-09 22:49:11 +0200

    Add project files.

---
+++ HomebreweryShoppingAssistaint/Models/HomebreweryArticleList.cs
@@ -0,0 +1,11 @@
+﻿namespace HomebreweryShoppingAssistaint.Models
+{
+    public class HomebreweryArticleList
+    {
+        public int ID { get; set; }
+        public string ProductName { get; set; }
+        public string ProductLink { get; set; }
+        public string ShopName { get; set; }
+        public decimal ProductPrice { get; set; } 
+    }
+}
