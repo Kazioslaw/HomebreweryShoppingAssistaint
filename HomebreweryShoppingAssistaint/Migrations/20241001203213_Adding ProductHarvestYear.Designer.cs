4274fdd1f01f428285936d6763170744e812c15e
Paweł Wilczyński <wi1czynskipaw31@gmail.com>
2024-10-03 21:32:51 +0200

    Dodanie brakujących migracji

--- HomebreweryShoppingAssistaint/Migrations/20241001203213_Adding ProductHarvestYear.Designer.cs
+++ HomebreweryShoppingAssistaint/Migrations/20241001203213_Adding ProductHarvestYear.Designer.cs
@@ -4,6 +4,7 @@ using HomebreweryShoppingAssistaint.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HomebreweryShoppingAssistaint.Migrations
 {
     [DbContext(typeof(HomebreweryShoppingAssistaintContext))]
-    partial class HomebreweryShoppingAssistaintContextModelSnapshot : ModelSnapshot
+    [Migration("20241001203213_Adding ProductHarvestYear")]
+    partial class AddingProductHarvestYear
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
