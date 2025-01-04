19d2750cd93b2f5e769082856936359fbd64f5f6
Paweł Wilczyński <wi1czynskipaw31@gmail.com>
2025-01-04 20:37:36 +0100

    Dodanie kontrolera dla fermentorów

---
+++ HomebreweryShoppingAssistaint/Controllers/FermentersController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HomebreweryShoppingAssistaint.Data;
+using HomebreweryShoppingAssistaint.Models;
+
+namespace HomebreweryShoppingAssistaint.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FermentersController : ControllerBase
+    {
+        private readonly HomebreweryShoppingAssistaintContext _context;
+
+        public FermentersController(HomebreweryShoppingAssistaintContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Fermenters
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Fermenter>>> GetFermenters()
+        {
+            return await _context.Fermenters.ToListAsync();
+        }
+
+        // GET: api/Fermenters/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Fermenter>> GetFermenter(long id)
+        {
+            var fermenter = await _context.Fermenters.FindAsync(id);
+
+            if (fermenter == null)
+            {
+                return NotFound();
+            }
+
+            return fermenter;
+        }
+
+        // PUT: api/Fermenters/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutFermenter(long id, Fermenter fermenter)
+        {
+            if (id != fermenter.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(fermenter).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FermenterExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Fermenters
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Fermenter>> PostFermenter(Fermenter fermenter)
+        {
+            _context.Fermenters.Add(fermenter);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetFermenter", new { id = fermenter.Id }, fermenter);
+        }
+
+        // DELETE: api/Fermenters/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteFermenter(long id)
+        {
+            var fermenter = await _context.Fermenters.FindAsync(id);
+            if (fermenter == null)
+            {
+                return NotFound();
+            }
+
+            _context.Fermenters.Remove(fermenter);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool FermenterExists(long id)
+        {
+            return _context.Fermenters.Any(e => e.Id == id);
+        }
+    }
+}
--- HomebreweryShoppingAssistaint/Models/Fermenter.cs
+++ HomebreweryShoppingAssistaint/Models/Fermenter.cs
@@ -1,9 +1,12 @@
 ﻿using HomebreweryShoppingAssistaint.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace HomebreweryShoppingAssistaint.Models
 {
     public class Fermenter
     {
+        [Key]
+        public long Id { get; set; }
         public int Number { get; set; }
         public string Name { get; set; }
         public EnumFermenterType Type { get; set; }
